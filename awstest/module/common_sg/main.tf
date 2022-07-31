cf44051547d1b824ef727e50fde81186507378e5
kum_cp <kum_cp@yahoo.com.vn>
2022-07-31 13:58:22 +0700

    fix some bug in try tf

--- awstest/module/common_sg/main.tf
+++ awstest/module/common_sg/main.tf
@@ -69,7 +69,7 @@ resource "aws_security_group_rule" "rules" {
   description       = "Specific access inbound for port ${each.value.port != null ? each.value.port : each.value.from_port}"
   from_port         = each.value.port != null ? each.value.port : each.value.from_port
   to_port           = each.value.port != null ? each.value.port : each.value.to_port
-  protocol          = try(each.value.protocol, "tcp")
+  protocol          = each.value.protocol != null ? each.value.protocol : "tcp"
   cidr_blocks       = try(each.value.cidr_blocks, null)
   ipv6_cidr_blocks  = try(each.value.ipv6_cidr_blocks, null)
   security_group_id = aws_security_group.allow_specific.id
