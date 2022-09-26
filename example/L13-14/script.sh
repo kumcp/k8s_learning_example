5f1dd4e88c49c68f3690ab82f055eb76302182b7
kum_cp <kum_cp@yahoo.com.vn>
2022-09-26 20:37:59 +0700

    fix some minor bugs

--- example/L13-14/script.sh
+++ example/L13-14/script.sh
@@ -1,12 +1,25 @@
 
+git clone https://github.com/kumcp/task-assign-app.git
+
+cd task-assign-app
+
+docker build -f ./Dockerfile -t php-web:latest .
+docker build -f ./deploy-helper/nginx/Dockerfile.nginx -t built-nginx:latest .
+
+
+
+git clone https://github.com/kumcp/k8s_learning_example.git
+
 
 kubectl create ns taa
 
 # Modify dpl.yaml
 # containers[0].name ->
 
-kubectl apply -f ./
+kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-1.11"
 
+
+kubectl apply -f ./
 
 # Setup ECR auto refresh token
 
@@ -18,11 +31,13 @@ kubectl create secret docker-registry regcred \
 
 # Note that login-password will expired in 12 hours -> need to run it again
 
-kubectl create secret docker-registry regcred \
-  --docker-server=666402361323.dkr.ecr.ap-southeast-1.amazonaws.com \
-  --docker-username=AWS \
-  --docker-password=$(aws ecr get-login-password) \
-  --namespace=taa
 
-
+# Run migration
 kubectl exec -it <pod-name> -n taa -- php artisan migrate
+kubectl exec -it <pod-name> -n taa -- php artisan db:seed --class=DatabaseSeeder
+
+
+# Jenkins note
+# 1. Create Credentials as Service Account
+# 2. Use jenkinsfile (from L12/lab1)
+# 3. Config credentials ID
