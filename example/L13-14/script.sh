c64a71ecf769a0f98e941daeb93a5c6a3b9e43e0
kum_cp <kum_cp@yahoo.com.vn>
2022-09-27 17:22:13 +0700

    update config for project

--- example/L13-14/script.sh
+++ example/L13-14/script.sh
@@ -6,12 +6,24 @@ cd task-assign-app
 docker build -f ./Dockerfile -t php-web:latest .
 docker build -f ./deploy-helper/nginx/Dockerfile.nginx -t built-nginx:latest .
 
+export ECR_IMAGE_REGION=<REGION_ID>
+export AWS_ACCOUNT_ID=<AWS_ACCOUNT_ID>
+
+aws ecr get-login-password --region $ECR_IMAGE_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$ECR_IMAGE_REGION.amazonaws.com
+
+docker tag built-nginx:latest $AWS_ACCOUNT_ID.dkr.ecr.$ECR_IMAGE_REGION.amazonaws.com/built-nginx:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$ECR_IMAGE_REGION.amazonaws.com/built-nginx:latest
+
+docker tag php-fpm:latest $AWS_ACCOUNT_ID.dkr.ecr.$ECR_IMAGE_REGION.amazonaws.com/php-fpm:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$ECR_IMAGE_REGION.amazonaws.com/php-fpm:latest
+
 
 
 git clone https://github.com/kumcp/k8s_learning_example.git
 
 
 kubectl create ns taa
+kubectl create ns adminer
 
 # Modify dpl.yaml
 # containers[0].name ->
@@ -19,16 +31,18 @@ kubectl create ns taa
 kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=release-1.11"
 
 
-kubectl apply -f ./
 
 # Setup ECR auto refresh token
 
 kubectl create secret docker-registry regcred \
-  --docker-server=${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com \
+  --docker-server=${AWS_ACCOUNT_ID}.dkr.ecr.${ECR_IMAGE_REGION}.amazonaws.com \
   --docker-username=AWS \
   --docker-password=$(aws ecr get-login-password) \
   --namespace=taa
 
+
+
+kubectl apply -f ./
 # Note that login-password will expired in 12 hours -> need to run it again
 
 
