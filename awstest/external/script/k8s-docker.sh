8c112f20b1be9bc767c93a63449d61d80d25aec2
kum_cp <kum_cp@yahoo.com.vn>
2023-05-23 15:58:55 +0700

    fix bug cannot setup cluster

--- awstest/external/script/k8s-docker.sh
+++ awstest/external/script/k8s-docker.sh
@@ -38,9 +38,9 @@ sudo mv ~/docker-compose /usr/local/bin/docker-compose
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >> ~/kubernetes.list
+sudo mv ~/kubernetes.list /etc/apt/sources.list.d
 
 echo "===========INSTALL KUBETLET & KUBEADM & KUBECTL ============="
 sudo apt-get update
