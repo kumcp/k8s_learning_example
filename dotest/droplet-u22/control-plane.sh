3fe0ddcb4931ab20d1d5f4da4dabd016822850b9
kum_cp <kum_cp@yahoo.com.vn>
2024-11-25 16:53:01 +0700

    update for windows generate key

--- dotest/droplet-u22/control-plane.sh
+++ dotest/droplet-u22/control-plane.sh
@@ -30,7 +30,7 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
 
-sudo mkdir /etc/apt/keyrings
+sudo mkdir -p /etc/apt/keyrings 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 
--- terraform/module/ssh-key/output.tf
+++ terraform/module/ssh-key/output.tf
@@ -1,7 +1,7 @@
 output "private_key" {
-  value = tls_private_key.pk.private_key_pem
+  value = local.is_linux ? tls_private_key.pk[0].private_key_pem : tls_private_key.pk_win[0].private_key_pem
 }
 
 output "public_key" {
-  value = tls_private_key.pk.public_key_openssh
+  value = local.is_linux ? tls_private_key.pk[0].public_key_openssh : tls_private_key.pk_win[0].public_key_openssh
 }
